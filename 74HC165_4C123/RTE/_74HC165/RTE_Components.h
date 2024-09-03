0d91aba4377519b97e34018fc25c7dc3bdb5bf88
Xiaolei Ren <30956889+xiaoleiren@users.noreply.github.com>
2024-09-03 22:49:59 +0800

    Add files via upload

---
+++ 74HC165_4C123/RTE/_74HC165/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '74HC165' 
+ * Target:  '74HC165' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "TM4C123.h"
+
+
+#endif /* RTE_COMPONENTS_H */

211e8a926b7b2b19a4b5e32f3065f1fee6d76791
Xiaolei Ren <30956889+xiaoleiren@users.noreply.github.com>
2024-09-03 23:02:01 +0800

    Add files via upload

---
+++ Projects and Examples/74HC595_4C123/RTE/_74HC595/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '74HC595' 
+ * Target:  '74HC595' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "LM4F120H5QR.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
